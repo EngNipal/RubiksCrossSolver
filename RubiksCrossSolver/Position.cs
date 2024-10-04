49d0b4b784b2f16ecba034d3a6ee2c483cdf6022
EngNipal <lovelas61@ya.ru>
2024-10-05 00:17:03 +0300

    Adds cross solving logic

--- RubiksCrossSolver/Position.cs
+++ RubiksCrossSolver/Position.cs
@@ -5,10 +5,38 @@
         public int Depth { get; private set; } = depth;
         public ulong Hash { get; private set; }
         public uint[] State { get; private set; } = state;
+        public List<Turn> Turns { get; private set; }
 
         public void SetHash(Zobrist zobrist)
         {
             Hash = zobrist.Hash(State);
+        }
+
+        public Turn? GetAntiturn()
+        {
+            return Turns?.Last() switch
+            {
+                Turn.R => Turn.Rp,
+                Turn.Rp => Turn.R,
+                Turn.R2 => Turn.R2,
+                Turn.U => Turn.Up,
+                Turn.Up => Turn.U,
+                Turn.U2 => Turn.U2,
+                Turn.F => Turn.Fp,
+                Turn.Fp => Turn.F,
+                Turn.F2 => Turn.F2,
+                Turn.L => Turn.Lp,
+                Turn.Lp => Turn.L,
+                Turn.L2 => Turn.L2,
+                Turn.D => Turn.Dp,
+                Turn.Dp => Turn.D,
+                Turn.D2 => Turn.D2,
+                Turn.B => Turn.Bp,
+                Turn.Bp => Turn.B,
+                Turn.B2 => Turn.B2,
+                null => null,
+                _ => throw new ArgumentOutOfRangeException(),
+            };
         }
     }
 }
--- RubiksCrossSolver/Program.cs
+++ RubiksCrossSolver/Program.cs
@@ -5,15 +5,70 @@
         static void Main(string[] args)
         {
             var zobrist = new Zobrist(48, 6);
-            var scramble = new Turns[] {Turns.R };
+            var scramble = new Turn[] { Turn.R };
             var rubiksCube = new RubiksCube(scramble);
 
             uint[] initialState = rubiksCube.GetCurrentState();
-            var initial = new Position(0, initialState);
+            int depth = 0;
+            var initial = new Position(depth, initialState);
             initial.SetHash(zobrist);
 
-            var positions = new Dictionary<ulong, Position>();            
-            positions.Add(initial.Hash, initial);
+            var positions = new Dictionary<ulong, Position>
+            {
+                { initial.Hash, initial }
+            };
+
+            var solved = new List<Position>();
+            while (depth < 9 || solved.Count == 0)
+            {            
+                foreach (var position in positions.Values.Where(x => x.Depth == depth))
+                {
+                    var antiturn = GetAntiturn(lastTurn.Value);
+                    foreach (Turn turn in Enum.GetValues(typeof(Turn)))
+                    {
+                        if (depth > 0 && turn == antiturn) continue;
+                        Position newPosition = CreatePosition(zobrist, depth, position.State, turn);
+                        if (positions.TryAdd(newPosition.Hash, newPosition))
+                        {
+                            newPosition.Turns.Add(turn);
+                            if (CrossIsSolved(newPosition.State))
+                            {
+                                solved.Add(newPosition);
+                            }
+                        }
+                    }
+                }
+
+                depth++;
+            }
+
+            foreach (var position in solved)
+            {
+                var solution = string.Join(", ", position.Turns);
+                Console.WriteLine(solution);
+            }
+        }
+
+        
+
+        private static Position CreatePosition(Zobrist zobrist, int depth, uint[] state, Turn turn)
+        {
+            uint[] newState = RubiksCube.GetStateByTurn(state, turn);
+            var newPosition = new Position(depth + 1, newState);
+            newPosition.SetHash(zobrist);
+            return newPosition;
+        }
+
+        private static bool CrossIsSolved(uint[] state)
+        {
+            return state[1] == (uint)Colour.White &&
+                state[3] == (uint)Colour.White &&
+                state[4] == (uint)Colour.White &&
+                state[6] == (uint)Colour.White &&
+                state[9] == (uint)Colour.Orange &&
+                state[17] == (uint)Colour.Green &&
+                state[25] == (uint)Colour.Red &&
+                state[33] == (uint)Colour.Blue;
         }
     }
 }
