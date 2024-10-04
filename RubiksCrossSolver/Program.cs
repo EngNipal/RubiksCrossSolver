093bcbe472fdb6b9827f872f854ed65c056e8e24
lovember <lovember@WIN-HNNSIA5ROMT>
2024-10-04 10:19:12 +0300

    Add project files.

---
+++ RubiksCrossSolver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RubiksCrossSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
