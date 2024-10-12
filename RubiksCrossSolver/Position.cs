6c273a126b12f19d62803659f3140eac4029c2fb
EngNipal <lovelas61@ya.ru>
2024-10-12 22:33:10 +0300

    Extends position constructor

--- RubiksCrossSolver/Position.cs
+++ RubiksCrossSolver/Position.cs
@@ -1,48 +1,46 @@
-﻿namespace RubiksCrossSolver
+﻿namespace RubiksCrossSolver;
+
+public class Position(int depth, uint[] state, List<Turn> turns)
 {
-    public class Position(int depth, uint[] state)
+    public int Depth { get; private set; } = depth;
+    public ulong Hash { get; private set; }
+    public uint[] State { get; private set; } = state;
+    public List<Turn> Turns { get; private set; } = [.. turns];
+
+    public void SetHash(Zobrist zobrist)
     {
-        public int Depth { get; private set; } = depth;
-        public ulong Hash { get; private set; }
-        public uint[] State { get; private set; } = state;
-        public List<Turn> Turns { get; private set; }
+        Hash = zobrist.Hash(State);
+    }
 
-        public void SetHash(Zobrist zobrist)
-        {
-            Hash = zobrist.Hash(State);
-        }
+    public void AddTurn(Turn turn)
+    {
+        Turns.Add(turn);
+    }
 
-        public void AddTurn(Turn turn)
+    public Turn? GetAntiturn()
+    {
+        return Turns?.Last() switch
         {
-            Turns ??= [];
-            Turns.Add(turn);
-        }
-
-        public Turn? GetAntiturn()
-        {
-            return Turns?.Last() switch
-            {
-                Turn.R => Turn.Rp,
-                Turn.Rp => Turn.R,
-                Turn.R2 => Turn.R2,
-                Turn.U => Turn.Up,
-                Turn.Up => Turn.U,
-                Turn.U2 => Turn.U2,
-                Turn.F => Turn.Fp,
-                Turn.Fp => Turn.F,
-                Turn.F2 => Turn.F2,
-                Turn.L => Turn.Lp,
-                Turn.Lp => Turn.L,
-                Turn.L2 => Turn.L2,
-                Turn.D => Turn.Dp,
-                Turn.Dp => Turn.D,
-                Turn.D2 => Turn.D2,
-                Turn.B => Turn.Bp,
-                Turn.Bp => Turn.B,
-                Turn.B2 => Turn.B2,
-                null => null,
-                _ => throw new ArgumentOutOfRangeException(),
-            };
-        }
+            Turn.R => Turn.Rp,
+            Turn.Rp => Turn.R,
+            Turn.R2 => Turn.R2,
+            Turn.U => Turn.Up,
+            Turn.Up => Turn.U,
+            Turn.U2 => Turn.U2,
+            Turn.F => Turn.Fp,
+            Turn.Fp => Turn.F,
+            Turn.F2 => Turn.F2,
+            Turn.L => Turn.Lp,
+            Turn.Lp => Turn.L,
+            Turn.L2 => Turn.L2,
+            Turn.D => Turn.Dp,
+            Turn.Dp => Turn.D,
+            Turn.D2 => Turn.D2,
+            Turn.B => Turn.Bp,
+            Turn.Bp => Turn.B,
+            Turn.B2 => Turn.B2,
+            null => null,
+            _ => throw new ArgumentOutOfRangeException(),
+        };
     }
 }
--- RubiksCrossSolver/Program.cs
+++ RubiksCrossSolver/Program.cs
@@ -1,83 +1,87 @@
-﻿namespace RubiksCrossSolver
+﻿namespace RubiksCrossSolver;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {            
-            var scramble = new Turn[] { Turn.R };
-            var rubiksCube = new RubiksCube(scramble);
-            uint[] initialState = rubiksCube.GetCurrentState();
-            List<Position> solves = GetSolves(initialState);
-            PrintSolution(solves);
-        }
+        string scramble = "L2, B, F2, D2, Bp, R2, F2, D2, L2, U2, Fp, R, Fp, D, U2, Rp, Fp, Up, F, R2";
+        var rubiksCube = new RubiksCube(scramble);
+        uint[] initialState = rubiksCube.GetCurrentState();
+        List<Position> solves = GetSolves(initialState);
+        PrintSolution(solves);
+        Console.ReadLine();
+    }
 
-        private static void PrintSolution(List<Position> solved)
+    private static void PrintSolution(List<Position> solved)
+    {
+        foreach (var position in solved)
         {
-            foreach (var position in solved)
-            {
-                var solution = string.Join(", ", position.Turns);
-                Console.WriteLine(solution);
-            }
+            var solution = string.Join(", ", position.Turns);
+            Console.WriteLine(solution);
         }
+    }
 
-        private static List<Position> GetSolves(uint[] initialState)
+    private static List<Position> GetSolves(uint[] initialState)
+    {
+        int depth = 0;
+        var zobrist = new Zobrist(48, 6);
+        var initial = new Position(depth, initialState, []);
+        initial.SetHash(zobrist);
+
+        var positions = new Dictionary<ulong, Position>
         {
-            int depth = 0;
-            var zobrist = new Zobrist(48, 6);
-            var initial = new Position(depth, initialState);
-            initial.SetHash(zobrist);
+            { initial.Hash, initial }
+        };
 
-            var positions = new Dictionary<ulong, Position>
+        var solves = new List<Position>();
+        while (depth < 11 && solves.Count == 0)
+        {
+            var deepPositions = positions.Values.Where(x => x.Depth == depth).ToList();
+            foreach (var position in deepPositions)
             {
-                { initial.Hash, initial }
-            };
-
-            var solves = new List<Position>();
-            while (depth < 9 && solves.Count == 0)
-            {
-                var deepPositions = positions.Values.Where(x => x.Depth == depth).ToList();
-                foreach (var position in deepPositions)
+                var antiturn = position.GetAntiturn();
+                foreach (Turn turn in Enum.GetValues(typeof(Turn)))
                 {
-                    var antiturn = position.GetAntiturn();
-                    foreach (Turn turn in Enum.GetValues(typeof(Turn)))
+                    if (depth > 0 && turn == antiturn) continue;
+                    Position newPosition = CreatePosition(zobrist, position, turn);
+                    if (positions.TryAdd(newPosition.Hash, newPosition))
                     {
-                        if (depth > 0 && turn == antiturn) continue;
-                        Position newPosition = CreatePosition(zobrist, depth, position.State, turn);
-                        if (positions.TryAdd(newPosition.Hash, newPosition))
+                        newPosition.AddTurn(turn);
+                        if (CrossIsSolved(newPosition.State))
                         {
-                            newPosition.AddTurn(turn);
-                            if (CrossIsSolved(newPosition.State))
-                            {
-                                solves.Add(newPosition);
-                            }
+                            solves.Add(newPosition);
                         }
                     }
+                    else
+                    {
+                        Console.WriteLine($"Позиция с хэшем {newPosition.Hash} уже есть в списке позиций");
+                    }
                 }
-
-                depth++;
             }
 
-            return solves;
+            depth++;
         }
 
-        private static Position CreatePosition(Zobrist zobrist, int depth, uint[] state, Turn turn)
-        {
-            uint[] newState = RubiksCube.GetStateByTurn(state, turn);
-            var newPosition = new Position(depth + 1, newState);
-            newPosition.SetHash(zobrist);
-            return newPosition;
-        }
+        return solves;
+    }
 
-        private static bool CrossIsSolved(uint[] state)
-        {
-            return state[1] == (uint)Colour.White &&
-                state[3] == (uint)Colour.White &&
-                state[4] == (uint)Colour.White &&
-                state[6] == (uint)Colour.White &&
-                state[9] == (uint)Colour.Orange &&
-                state[17] == (uint)Colour.Green &&
-                state[25] == (uint)Colour.Red &&
-                state[33] == (uint)Colour.Blue;
-        }
+    private static Position CreatePosition(Zobrist zobrist, Position position, Turn turn)
+    {
+        uint[] newState = RubiksCube.GetStateByTurn(position.State, turn);
+        var newPosition = new Position(position.Depth + 1, newState, position.Turns);
+        newPosition.SetHash(zobrist);
+        return newPosition;
+    }
+
+    private static bool CrossIsSolved(uint[] state)
+    {
+        return state[1] == (uint)Colour.White &&
+            state[3] == (uint)Colour.White &&
+            state[4] == (uint)Colour.White &&
+            state[6] == (uint)Colour.White &&
+            state[9] == (uint)Colour.Orange &&
+            state[17] == (uint)Colour.Green &&
+            state[25] == (uint)Colour.Red &&
+            state[33] == (uint)Colour.Blue;
     }
 }
